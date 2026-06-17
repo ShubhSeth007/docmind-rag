3c4f7955cbd2edd8244414c3c215fcd48a2559de
ShubhSeth007 <shubhseth478@gmail.com>
2026-06-18 01:23:40 +0530

    Update download_models.py

--- download_models.py
+++ download_models.py
@@ -1,9 +1,5 @@
-from sentence_transformers import SentenceTransformer, CrossEncoder
+from sentence_transformers import SentenceTransformer
 
 print("Downloading embedding model...")
 SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-
-print("Downloading reranker model...")
-CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
-
 print("Models downloaded successfully.")
